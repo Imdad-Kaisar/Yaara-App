d9bb9cb18e76f6f9da77d07d42855d84e5ba4054
Imdad-Kaisar <ikaisar345@gamil.com>
2024-06-16 10:03:57 +0530

    working fine

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -2,16 +2,24 @@ import React from 'react'
 import Messages from './Messages'
 import MessageInput from './MessageInput'
 import { TiMessages } from "react-icons/ti";
+import { useEffect } from "react";
+import useConversation from "../../zustand/useConversation";
+import { useAuthContext } from "../../context/AuthContext";
 
 const MessageContainer = () => {
-    const noChatSelected= false;
+    const { selectedConversation, setSelectedConversation } = useConversation();
+	useEffect(() => {
+		// cleanup function (unmounts)
+		return () => setSelectedConversation(null);
+	}, [setSelectedConversation]);
+	
   return (
     <div className='md:min-w-[450px] flex flex-col'>
-        {noChatSelected ? <NoChatSelected/> :(
+        {!selectedConversation ? <NoChatSelected/> :(
         <>
  				{/* Header */}
  				<div className='bg-slate-500 px-4 py-2 mb-2'>
- 					<span className='label-text'>To:</span> <span className='text-gray-900 font-bold'>John doe</span>
+ 					<span className='label-text'>To:</span> <span className='text-gray-900 font-bold'>{selectedConversation.fullName}</span>
 				</div>
 
 				<Messages />
